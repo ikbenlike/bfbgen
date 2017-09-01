65656cbf34c8397888dd1e2d8bb662a41143e9f8
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-09-01 23:49:10 +0200

    adds putchar function

--- src/prefix.c
+++ src/prefix.c
@@ -13,4 +13,12 @@
 #error "This needs to be compiled with a ix86-elf compiler"
 #endif
 
+#include "vga.h"
+
+void putchar(char c){
+    vga_write(&c, 1);
+}
+
 void kernel_bfmain(void){
+    vga_initialize();
+    char array[30000] = {0};
