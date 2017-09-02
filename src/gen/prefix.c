8aeae243e57861d699662798a8e00285c7012cce
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-09-02 16:04:03 +0200

    updated gitignore and made it so that prefix.c actually compiles

---
+++ src/gen/prefix.c
@@ -0,0 +1,25 @@
+//
+// Prefix for bf2c code generation phase
+// Provides kernel_bfmain function to be called
+// by boot.s and declares the array the
+// brainfuck code needs to function
+//
+
+#if defined(__linux__)
+#error "You are not using a cross-compiler, you will most certainly run into trouble"
+#endif
+ 
+#if !defined(__i386__)
+#error "This needs to be compiled with a ix86-elf compiler"
+#endif
+
+#include "vga_driver.h"
+
+void putchar(char c){
+    vga_write(&c, 1);
+}
+
+void kernel_bfmain(void){
+    vga_initialize();
+    char array[30000] = {0};
+    int dptr = 0;
