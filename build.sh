807e0066b82ee608af20c26a73d7b50faa63018e
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-09-02 20:52:16 +0200

    build.sh now moves bfbgen.kernel to the right location

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 ./bin/bf2c $1 --prefix src/gen/prefix.c --postfix src/gen/postfix.c -o src/kernel/kernel/kernel.c
 cd src/kernel
+make clean
 make
-cd ..
 mv bfbgen.kernel ../isodir/boot
+cd ..
+ls
 ./mk_iso.sh
 mv builds/bfbgen.iso ../
