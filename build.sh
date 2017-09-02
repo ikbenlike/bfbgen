48d550f2ec8bda42dd8fc77de4cde8d31b285c38
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-09-02 17:19:27 +0200

    updates .gitignore so it includes grub.cfg and adds a script that generates an ISO, updates build.sh to run this script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 ./bin/bf2c $1 --prefix src/gen/prefix.c --postfix src/gen/postfix.c -o src/kernel/kernel/kernel.c
 cd src/kernel
 make
-mv bfbgen.kernel ../..
+cd ..
+mv bfbgen.kernel ../isodir/boot
+./mk_iso.sh
+mv builds/bfbgen.iso ../
---
+++ src/mk_iso.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+grub-mkrescue -o builds/bfbgen.iso isodir
+
